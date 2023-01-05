2149a74865020e351f0486244c712009e6715d58
Srujan Gurram <srujangs8@gmail.com>
2023-01-05 22:00:05 +0530

    changed linked wikis to array type

--- src/types/wiki.ts
+++ src/types/wiki.ts
@@ -203,7 +203,7 @@ export interface Wiki {
   media?: Media[];
   user: User;
   metadata: MData[];
-  linkedWikis?: LinkedWikis;
+  linkedWikis?: LinkedWikis[];
   version: number;
   language: LanguagesISOEnum;
   updated?: string;
