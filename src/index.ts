6664b15918407f4de379ea17cc393e66c5c4fd12
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-01-10 10:56:33 +0100

    centralise feature export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./lib/wiki-score";
 export * from "./lib/check-deep-equal";
 export * from "./lib/check-wiki-validity";
+export * from "./lib/twitter-api-client";
 export * from "./data/constants";
 export * from "./schema";
